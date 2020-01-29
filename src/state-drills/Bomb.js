3bb3087dafa5f76c824622f06e7bc2bf68a1ab55
Jose <jozylopez27@gmail.com>
2020-01-29 13:36:45 -0600

    drill 2 complete

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,41 +1,5 @@
 import React from 'react';
 
-// export default class Bomb extends React.Component {
-//     state= {
-//         count: 0
-//     }
-//     componentDidMount() {let interval = setInterval(() => {
-//         this.setState({
-//         count: this.state.count+=1
-//         })
-//     }, 1000)}
-//     render() {
-//         return (
-//             <div>
-//             </div>
-//         )
-//     }
-// }
-
-
-// export default class Bomb extends React.Component {
-//     state= {
-//         count: 0
-//     }
-//     componentDidMount() {this.interval = setInterval(() => {
-//         this.setState({
-//         count: this.state.count + 1
-//         })
-//     }, 1000)}
-//     render() {
-//         return (
-//             <div>
-//                 <p>tick</p>
-//             </div>
-//         )
-//     }
-// }
-
 export default class Bomb extends React.Component {
     state= {
         count: 0
@@ -45,11 +9,26 @@ export default class Bomb extends React.Component {
         count: this.state.count + 1
         })
     }, 1000)}
+
+    createEl() {
+        if(this.state.count % 2 === 0) {
+            return 'tick'
+        }else if(this.state.count >= 8) {
+            clearInterval(this.interval)
+            return 'BOOM!!!'
+        } else {
+            return 'tock'
+        }
+    }
+
     render() {
         return (
-            <div>
-                {this.state.count % 2 ? <p>tick</p> : this.state.count >= 8 ? <p>BOOM!!!</p> : <p>tock</p>}
-            </div>
+        <div>
+       <p>{this.createEl()}</p>
+       </div>
         )
+    }
+    componentWillUnmount(){
+        clearInterval(this.interval)
     }
 }
