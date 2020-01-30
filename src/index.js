57b1a029d974227ae4730f4de06c846eb4eac774
Jose <jozylopez27@gmail.com>
2020-01-30 09:58:14 -0600

    noth

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
+import Accordian from './state-drills/Accordion'
 import './index.css';
 
-ReactDOM.render( <Bomb /> , document.getElementById('root'));
+ReactDOM.render( <Accordian /> , document.getElementById('root'));
 
 {/* <App /> */}
 
--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,30 +1,86 @@
 import React from 'react'
 
+// export default class Accordion extends React.Component {
+//     static defaultProps = {
+//         sections: [
+//             {
+//               title: 'Section 1',
+//               content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+//             },
+//             {
+//               title: 'Section 2',
+//               content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+//             },
+//             {
+//               title: 'Section 3',
+//               content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+//             },
+//           ]
+//     };
+
+//     state = {
+//       expanded: false
+//     }
+
+//     handleClick = () => {
+//       this.setState({
+//         expanded: !this.state.expanded
+//       })
+//     }
+
+//     render() {
+//       if(this.state.expanded) {
+//         return <ul>
+//         {this.props.sections.map((el) =>
+//         <li><button onClick={() => this.handleClick(el.content)}>{el.title}</button>
+//         <p>{el.content}</p></li>)}
+//         </ul>
+//       } else if(!this.state.expanded)
+//         return(
+//             <ul>
+//                 {this.props.sections.map((el) =>
+//                 <li><button onClick={() => this.handleClick(el.content)}>{el.title}</button></li>)}
+//             </ul>
+//         )
+//     }
+// }
+
+
+
 export default class Accordion extends React.Component {
-    static defaultProps = {
-        sections: [
-            {
-              title: 'Section 1',
-              content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-            },
-            {
-              title: 'Section 2',
-              content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-            },
-            {
-              title: 'Section 3',
-              content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-            },
-          ]
-    };
+  static defaultProps = {
+      sections: [
+          {
+            title: 'Section 1',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+          },
+          {
+            title: 'Section 2',
+            content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+          },
+          {
+            title: 'Section 3',
+            content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+          },
+        ]
+  };
 
-    render() {
-        return(
-            <ul>{this.props.sections.map((section)) => {
-            <li type="button"> Title = {section.title} </li> 
-        }
-            </ul>
-        )
-    }
+  state = {
+    content: ''
+  }
 
+  handleClick = (change) => {
+    this.setState({
+      content: change
+    })
+  }
+
+  render() {
+      return <ul>
+      {this.props.sections.map((el) =>
+      <li><button onClick={() => this.handleClick(el.content)}>{el.title}</button>
+      </li>)}
+      <p>{this.state.content}</p>
+      </ul>
+}
 }
